41ac39531bf6636f5ab76a7a68eff1e46f731b69
vikyyahya <viky@lenna.ai>
2020-02-28 22:59:08 +0700

    comt first

---
+++ admin/srai_lookup.php
@@ -0,0 +1,252 @@
+<?php
+/***************************************
+ * http://www.program-o.com
+ * PROGRAM O
+ * Version: 2.6.11
+ * FILE: srai_lookup.php
+ * AUTHOR: Elizabeth Perreau and Dave Morton
+ * DATE: 05-26-2014
+ * DETAILS: Manage entries in the srai_lookup table
+ ***************************************/
+
+$post_vars = filter_input_array(INPUT_POST);
+$get_vars = filter_input_array(INPUT_GET);
+
+$editScriptTemplate = '<script type="text/javascript" src="scripts/[editScript].js"></script>';
+$editScript = 'srai_lookup';
+$editScriptTag = str_replace('[editScript]', $editScript, $editScriptTemplate);
+
+$form_vars = array_merge((array)$post_vars, (array)$get_vars);
+//exit('Form vars:<pre>' . PHP_EOL . print_r($form_vars, true));
+$action = (isset($form_vars['action'])) ? $form_vars['action'] : '';
+
+$group = (isset($get_vars['group'])) ? $get_vars['group'] : 1;
+
+$mainContent = $template->getSection('editSRAIPage');
+$msg = (empty($action)) ? '' : $action();
+
+$mainContent = str_replace('[group]', $group, $mainContent);
+
+$topNav         = $template->getSection('TopNav');
+$leftNav        = $template->getSection('LeftNav');
+$rightNav       = $template->getSection('RightNav');
+$main           = $template->getSection('Main');
+$navHeader      = $template->getSection('NavHeader');
+$FooterInfo     = getFooter();
+
+$errMsgClass    = (!empty($msg)) ? "ShowError" : "HideError";
+$errMsgStyle    = $template->getSection($errMsgClass);
+
+$noLeftNav      = '';
+$noTopNav       = '';
+
+$noRightNav     = $template->getSection('NoRightNav');
+
+$headerTitle    = 'Actions:';
+$pageTitle      = 'My-Program O - SRAI Lookup';
+$mainTitle      = 'SRAI Lookup ' . $template->getSection('HelpLink');
+
+$showHelp = $template->getSection('editSRAIShowHelp');
+$mainContent = str_replace('[showHelp]', $showHelp, $mainContent);
+
+$countSQL = 'SELECT COUNT(id) FROM srai_lookup WHERE bot_id = :bot_id;';
+$params = array(':bot_id' => $bot_id);
+$countResult = db_fetch($countSQL, $params, __FILE__, __FUNCTION__, __LINE__);
+$row_count = number_format($countResult['COUNT(id)']);
+
+$mainContent = str_replace('[row_count]', $row_count, $mainContent);
+$mainContent = str_replace('[bot_name]', $bot_name, $mainContent);
+
+/**
+ * Function fillLookup
+ *
+ * @return string
+ */
+function fillLookup()
+{
+    set_time_limit(0);
+    global $bot_id;
+    // <srai>XEDUCATELANG <star index="1"/> XSPLIT <star index="2"/> XSPLIT <star index="3"/>XSPLIT</srai>
+    $starArray = array('~<star[ ]?/>~i', '~<star index="\d+"[ ]?\/>~');
+    $msg = '';
+    $timeStart = microtime(true);
+    // Drop the index on the table srai_lookup to speed things up
+
+    try
+    {
+        // first, check to see if the index exists
+        $testSQL = "SHOW INDEX FROM `srai_lookup` WHERE `Key_name` = 'pattern';";
+        $testResult = db_fetch($testSQL, null, __FILE__, __FUNCTION__, __LINE__);
+
+        // if the index exists then drop it. we'll recreate it later
+        if (!empty($testResult))
+        {
+            $dropSQL = 'ALTER TABLE srai_lookup DROP INDEX pattern;';
+            $dropResult = db_write($dropSQL, null, false, __FILE__, __FUNCTION__, __LINE__);
+        }
+
+        // now delete the rows for the current chatbot, but leave the others alone
+        $deleteSQL = 'delete from `srai_lookup` where bot_id=:bot_id;';
+        $deleteParams = array(':bot_id' => $bot_id);
+        $deleteResult = db_write($deleteSQL, $deleteParams, false, __FILE__, __FUNCTION__, __LINE__);
+        $deleteResult = number_format($deleteResult);
+
+        // lastly, check if the aiml table has an index for srai search and if not, add it
+        //
+        $testSQL = "SHOW INDEX FROM `aiml` WHERE `Key_name` = 'srai_search';";
+        $testResult = db_fetch($testSQL, null, __FILE__, __FUNCTION__, __LINE__);
+
+        // if the index exists then drop it, then recreate it
+        if (!empty($testResult))
+        {
+            $dropSQL = 'ALTER TABLE aiml DROP INDEX srai_search; ALTER TABLE `aiml` ADD INDEX `srai_search` (`bot_id`, `pattern`(64));';
+            $dropResult = db_write($dropSQL, null, false, __FILE__, __FUNCTION__, __LINE__);
+        }
+        else
+        {
+            $dropSQL = 'ALTER TABLE `aiml` ADD INDEX `srai_search` (`bot_id`, `pattern`(64));';
+            $dropResult = db_write($dropSQL, null, false, __FILE__, __FUNCTION__, __LINE__);
+        }
+        $let = $timeStart;
+        $now = microtime(true);
+        $cet = round($now - $let, 3);
+        $let = $now;
+        $msg .= "Removed {$deleteResult} existing rows from the srai lookup table, and altered the indexes.";
+        $msg .= "Elapsed: {$cet} seconds.<br/>";
+    }
+    catch (Exception $e) { }
+
+    $searchSQL = "select id, template from aiml where template like '%<srai>%' and bot_id = :bot_id order by id asc;";
+    $searchParams = array(
+        ':bot_id' => $bot_id
+    );
+    $searchResult = db_fetchAll($searchSQL, $searchParams, __FILE__, __FUNCTION__, __LINE__);
+    $rowCount = count($searchResult);
+    $totalRows = number_format($rowCount);
+    $now = microtime(true);
+    $cet = round($now - $let, 3);
+    $let = $now;
+    $msg .= ("Found $totalRows AIML categories that contain SRAI calls. Elapsed: $cet seconds.<br>\n");
+    //exit();
+    $patterns = array(); // array to contain valid patterns, to prevent duplicates
+
+    foreach ($searchResult as $row)
+    {
+        $id = $row['id'];
+
+        if (!isset($patterns[$id]))
+        {
+            $patterns[$id] = array();
+        }
+
+        $AIMLtemplate = trim($row['template']);
+
+        while (stripos($AIMLtemplate, '<srai>', 0) !== false)
+        {
+            $start = stripos($AIMLtemplate, '<srai>', 0);
+            $end = stripos($AIMLtemplate, '</srai>', $start);
+            $len = $end - $start;
+
+            $srai = substr($AIMLtemplate, $start, $len);
+            $srai = preg_replace($starArray, '*', $srai); // replace references to <star> with the * wildcard
+            $srai = _strtoupper($srai);
+            $srai = trim(str_ireplace('<SRAI>', '', $srai));
+
+            if (strstr($srai, '<') == false)
+            {
+                if (!in_array($srai, $patterns[$id]))
+                {
+                    $patterns[$id][] = $srai;
+                }
+            }
+            $AIMLtemplate = substr($AIMLtemplate, $end);
+        }
+    }
+
+    //save_file(_LOG_PATH_ . 'srai_lookup.patterns.txt', print_r($patterns, true));
+    foreach ($patterns as $key => $value)
+    {
+        if (empty($value)) unset($patterns[$key]);
+    }
+
+    /** @noinspection SqlDialectInspection */
+    $patternSQL = "SELECT id FROM aiml WHERE bot_id = :bot_id AND pattern like :pattern ORDER BY id limit 1;";
+    $lookups = array();
+    foreach ($patterns as $id => $row)
+    {
+        if (empty($row)) continue;
+        foreach ($row as $index => $pattern)
+        {
+            $params = array(
+                ':bot_id' => $bot_id,
+                ':pattern' => $pattern
+            );
+            $patternResult = db_fetch($patternSQL, $params, __FILE__, __FUNCTION__, __LINE__);
+            $template_id = $patternResult['id'];
+            $lookups[] = array(
+                'bot_id' => $bot_id,
+                'pattern' => $pattern,
+                'template_id' => $template_id
+            );
+/*
+*/
+        }
+    }
+    foreach ($lookups as $key => $value)
+    {
+        if (empty($value['template_id'])) {
+            unset($lookups[$key]);
+        }
+    }
+    $now = microtime(true);
+    $cet = round($now - $let, 3);
+    $let = $now;
+    $lookupsCount = count($lookups);
+    $lookupsCount = number_format($lookupsCount);
+    $msg .= ("Loaded {$lookupsCount} AIML categories that need to be added to the lookup table. Elapsed: {$cet} seconds.<br>\n");
+    //save_file(_LOG_PATH_ . 'lookups.txt', print_r($lookups, true));
+    /** @noinspection SqlDialectInspection */
+    // db_multi_insert($tableName, $data,  $file = 'unknown', $function = 'unknown', $line = 'unknown')
+    $insertResult = db_multi_insert('srai_lookup', $lookups, __FILE__, __FUNCTION__, __LINE__);
+    $insertCount = number_format($insertResult);
+
+    // Now put the index back, and remove the temporary one
+    /** @noinspection SqlDialectInspection */
+    $indexSQL = 'ALTER TABLE `srai_lookup` ADD INDEX `pattern` (`bot_id`, `pattern`(64));ALTER TABLE aiml DROP INDEX srai_search;';
+    $indexResult = db_write($indexSQL, null, false, __FILE__, __FUNCTION__, __LINE__);
+    $now = microtime(true);
+    $cet = round($now - $let, 3);
+    $let = $now;
+    $msg .= "Inserted $insertCount new entries into the SRAI lookup table. Elapsed: {$cet}<br>\n";
+    $timeEnd = microtime(true);
+    $elapsed = round($timeEnd - $timeStart, 3);
+    $msg .= "Total elapsed time: $elapsed seconds.<br>\n";
+
+    return $msg;
+}
+
+function foo($patterns, $lookups)
+{
+    foreach ($patterns as $id => $row)
+    {
+        foreach ($row as $pattern)
+        {
+            if (is_array($pattern))
+            {
+                $lookups = foo($pattern, $lookups);
+            }
+            $params = array(
+                ':bot_id' => $bot_id,
+                ':pattern' => $pattern
+            );
+            $patternResult = db_fetch($patternSQL, $params, __FILE__, __FUNCTION__, __LINE__);
+            $template_id = $patternResult['id'];
+            $lookups[] = array(
+                ':bot_id' => $bot_id,
+                ':pattern' => $pattern,
+                ':template_id' => $template_id
+            );
+        }
+    }
+    return $lookups;
+}
---
+++ gui/plain/index.php
@@ -0,0 +1,152 @@
+<?php
+
+/***************************************
+ * http://www.program-o.com
+ * PROGRAM O
+ * Version: 2.6.11
+ * FILE: gui/plain/index.php
+ * AUTHOR: Elizabeth Perreau and Dave Morton
+ * DATE: MAY 17TH 2014
+ * DETAILS: simple example gui
+ ***************************************/
+$display = "";
+$thisFile = __FILE__;
+
+if (!file_exists('../../config/global_config.php'))
+{
+    header('Location: ../../install/install_programo.php');
+}
+
+/** @noinspection PhpIncludeInspection */
+require_once('../../config/global_config.php');
+session_name('ProgramO');
+$debug_div = '';
+$hideSP = '';
+$resizeResponseDiv = '';
+$clearButton = '';
+$get_vars = (!empty($_GET)) ? filter_input_array(INPUT_GET) : array();
+$post_vars = (!empty($_POST)) ? filter_input_array(INPUT_POST) : array();
+$form_vars = array_merge($post_vars, $get_vars); // POST overrides and overwrites GET
+if (!empty($form_vars)) require_once('../../chatbot/conversation_start.php');
+  $bot_id = (!empty($form_vars['bot_id'])) ? $form_vars['bot_id'] : 1;
+$say = (!empty($form_vars['say'])) ? $form_vars['say'] : '';
+$convo_id = (isset($form_vars['convo_id'])) ? $form_vars['convo_id'] : md5(time());
+$format = (!empty($form_vars['format'])) ? _strtolower($form_vars['format']) : 'html';
+
+if (ERROR_DEBUGGING)
+{
+    $convo_id = (isset($form_vars['convo_id'])) ? $form_vars['convo_id'] : 'DEBUG'; // Hard-code the convo_id during debugging
+    $debug_src = (!empty($form_vars) && file_exists(_DEBUG_PATH_ . "{$convo_id}.txt")) ? _DEBUG_URL_ . "reader.php?file={$convo_id}.txt" : '';
+    $debug_div = <<<endDebugDiv
+
+<iframe id="debugDiv" src="$debug_src" frameborder="0">
+endDebugDiv;
+    $hideSP = 'display: none;';
+    $resizeResponseDiv = 'max-height: 200px;';
+    $clearButton = <<<endClr
+<input id="btnClear" name="" type="button" value="Clear Div" onclick="document.getElementById('responses').innerHTML = '';">
+endClr;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Program O AIML PHP Chatbot</title>
+    <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon"/>
+    <meta name="Description" content="A Free Open Source AIML PHP MySQL Chatbot called Program-O. Version2"/>
+    <meta name="keywords" content="Open Source, AIML, PHP, MySQL, Chatbot, Program-O, Version2"/>
+    <style type="text/css">
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        #responses {
+            width: 90%;
+            min-width: 515px;
+            height: auto;
+            min-height: 150px;
+            max-height: 500px;
+            overflow: auto;
+            border: 3px inset #666;
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 1em;
+            padding: 5px;
+            box-sizing: border-box;
+            <?php echo $resizeResponseDiv ?>
+        }
+        #debugDiv {
+            width: 90%;
+            min-height: 1.2em;
+            height: 55vh;
+            border: 3px inset #666;
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+        }
+
+        #input {
+            width: 90%;
+            min-width: 535px;
+            margin-bottom: 15px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        #shameless_plug {
+            position: absolute;
+            right: 10px;
+            bottom: 10px;
+            border: 1px solid red;
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-shadow: 2px 2px 2px 0 #808080;
+            padding: 5px;
+            border-radius: 5px;
+            <?php echo $hideSP ?>
+        }
+
+
+        #convo_id {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            border: 1px solid red;
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-shadow: 2px 2px 2px 0 #808080;
+            padding: 5px;
+            border-radius: 5px;
+        }
+
+    </style>
+</head>
+<body onload="document.getElementById('say').focus()">
+<h3>Program O Example GUI Page - HTML</h3>
+<!-- The DIV below is for debugging purposes, and can be safely removed, if desired. -->
+<div id="convo_id">Conversion ID: <?php echo $convo_id; ?></div>
+<form name="chatform" method="post" action="index.php#end"
+      onsubmit="if(document.getElementById('say').value == '') return false;">
+    <div id="input">
+        <label for="say">Say:</label>
+        <input type="text" name="say" id="say" size="70"/>
+        <input type="submit" name="submit" id="btn_say" value="say"/>
+        <input type="hidden" name="convo_id" id="convo_id" value="<?php echo $convo_id; ?>"/>
+        <input type="hidden" name="bot_id" id="bot_id" value="<?php echo $bot_id; ?>"/>
+        <input type="hidden" name="format" id="format" value="<?php echo $format; ?>"/>
+        <?php echo $clearButton ?>
+    </div>
+</form>
+<div id="responses">
+    <?php echo $display . '<div id="end">&nbsp;</div>' . PHP_EOL ?>
+</div>
+<?php echo $debug_div ?>
+<div id="shameless_plug">
+    To get your very own chatbot, visit <a href="http://www.program-o.com">program-o.com</a>!
+</div>
+</body>
+</html>
